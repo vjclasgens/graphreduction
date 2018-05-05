5c5dee000b5c3fa09ae533c06945ebe99478fcf2
V.J. Clasgens <vjclasgens@gmail.com>
2018-05-05 10:09:41 -0600

    simplify by using RedGreenNode as a graph

--- src/main/java/com/graphtest/graphreduction/RedGreenNode.java
+++ src/main/java/com/graphtest/graphreduction/RedGreenNode.java
@@ -1,14 +1,21 @@
 package com.graphtest.graphreduction;
 
 public class RedGreenNode {
+    public enum NodeType {RED, GREEN, GRAPH}
+
     private String name;
-    private String color;
+    private NodeType nodeType;
     private RedGreenNode[] childrenNodes;
 
-    public RedGreenNode(String name, String color, RedGreenNode[] childrenNodes) {
+    public RedGreenNode(String name, NodeType nodeType, RedGreenNode[] childrenNodes) {
         this.name = name;
-        this.color = color;
+        this.nodeType = nodeType;
         this.childrenNodes = childrenNodes;
+    }
+
+    // this is the method where we'll solve the problem
+    public RedGreenNode[] reduceGraph(RedGreenNode[] graph) {
+        return graph;
     }
 
     public String getName() {
@@ -19,12 +26,12 @@ public class RedGreenNode {
         this.name = name;
     }
 
-    public String getColor() {
-        return color;
+    public NodeType getColor() {
+        return nodeType;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    public void setColor(NodeType color) {
+        this.nodeType = color;
     }
 
     public RedGreenNode[] getChildrenNodes() {
@@ -35,8 +42,20 @@ public class RedGreenNode {
         this.childrenNodes = childrenNodes;
     }
 
+    public String getChildrenString() {
+        // use stringbuilder to reduce space b/c strings are immutable
+        StringBuilder sb = new StringBuilder();
+        for(RedGreenNode rgn : childrenNodes) {
+            sb.append(rgn.toString());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
     @Override
     public String toString() {
         return name;
     }
+
+
 }
--- src/test/java/com/graphtest/graphreduction/TestRedGreenGraph.java
+++ src/test/java/com/graphtest/graphreduction/TestRedGreenGraph.java
@@ -3,34 +3,37 @@ package com.graphtest.graphreduction;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import com.graphtest.graphreduction.RedGreenNode.NodeType;
 
 public class TestRedGreenGraph {
-
-    RedGreenGraph redGreenGraph;
+    RedGreenNode redGreenGraph;
 
     @Before
     public void initializeTestData() {
         // create test data input from the provided example (see src/inputfiles/input1.txt)
-        redGreenGraph = new RedGreenGraph(createInitialInputData());
+        redGreenGraph = new RedGreenNode("graph", NodeType.GRAPH, createInitialInputData());
     }
 
     @Test
     public void testSimpleReduction() {
         String expected = "A\n"+"B\n"+"C\n"+"D\n";
-        RedGreenGraph result = new RedGreenGraph(redGreenGraph.reduceGraph(redGreenGraph.getRedGreenNodes()));
-        Assert.assertEquals(expected, result);
+
+        RedGreenNode result = new RedGreenNode("graph", NodeType.GRAPH,
+                redGreenGraph.reduceGraph(redGreenGraph.getChildrenNodes()));
+
+        Assert.assertEquals(expected, result.getChildrenString());
     }
 
     private RedGreenNode[] createInitialInputData() {
         // TODO: Generate these objects by reading from a txt file
         // create RedGreenNodes based on input
-        RedGreenNode nodeA = new RedGreenNode("A","green", null);
-        RedGreenNode nodeB = new RedGreenNode("B","green", null);
-        RedGreenNode nodeC = new RedGreenNode("C","red", null);
-        RedGreenNode nodeD = new RedGreenNode("D","red", null);
-        RedGreenNode nodeE = new RedGreenNode("E","green", null);
-        RedGreenNode nodeF = new RedGreenNode("F","red", null);
-        RedGreenNode nodeG = new RedGreenNode("G","red", null);
+        RedGreenNode nodeA = new RedGreenNode("A", NodeType.GREEN, null);
+        RedGreenNode nodeB = new RedGreenNode("B", NodeType.GREEN, null);
+        RedGreenNode nodeC = new RedGreenNode("C", NodeType.RED, null);
+        RedGreenNode nodeD = new RedGreenNode("D", NodeType.RED, null);
+        RedGreenNode nodeE = new RedGreenNode("E", NodeType.GREEN, null);
+        RedGreenNode nodeF = new RedGreenNode("F", NodeType.RED, null);
+        RedGreenNode nodeG = new RedGreenNode("G", NodeType.RED, null);
 
         // assign direction to RedGreenNodes based on input
         nodeB.setChildrenNodes(new RedGreenNode[] {nodeA, nodeD});
